3c19fe84415136da1279cf1afe2387e31008ae5a
Felix Leupold <felixleupold90@gmail.com>
2021-03-23 12:07:36 +0100

    Sign and place order

--- src/make_trade.ts
+++ src/make_trade.ts
@@ -1,12 +1,21 @@
+import {
+  OrderKind,
+  Order,
+  signOrder,
+  SigningScheme,
+  domain,
+} from "@gnosis.pm/gp-v2-contracts";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { HardhatEthersHelpers } from "@nomiclabs/hardhat-ethers/types";
 import ERC20 from "@openzeppelin/contracts/build/contracts/ERC20.json";
 import { TokenInfo, TokenList } from "@uniswap/token-lists";
-import { BigNumber, Contract } from "ethers";
+import { BigNumber, Contract, ethers } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import fetch from "node-fetch";
 
-async function makeTrade(
+import { Api } from "./api";
+
+export async function makeTrade(
   tokenListUrl: string,
   { ethers, network }: HardhatRuntimeEnvironment
 ): Promise<void> {
@@ -15,28 +24,70 @@ async function makeTrade(
     throw "Network doesn't expose a chainId";
   }
 
-  const all_tokens = await fetchTokenList(tokenListUrl, network.config.chainId);
-  const tokens_with_balance = await tokensWithBalance(
-    all_tokens,
+  const allTokens = await fetchTokenList(tokenListUrl, network.config.chainId);
+  const tokensWithBalance = await filterTokensWithBalance(
+    allTokens,
     trader,
     ethers
   );
-  if (tokens_with_balance.length === 0) {
+  if (tokensWithBalance.length === 0) {
     throw "Account doesn't have any balance in any of the provided token";
   }
 
-  const { token: sell_token, balance: sell_balance } = selectRandom(
-    tokens_with_balance
+  const { token: sellToken, balance: sellBalance } = selectRandom(
+    tokensWithBalance
   );
-  const buy_token = selectRandom(
-    all_tokens.filter((token) => sell_token !== token)
+  const buyToken = selectRandom(
+    allTokens.filter((token) => sellToken !== token)
+  );
+
+  const api = new Api(network.name);
+  const fee = await api.getFee(
+    sellToken.address,
+    buyToken.address,
+    sellBalance,
+    OrderKind.SELL
+  );
+
+  if (sellBalance.lte(fee)) {
+    throw "Account doesn't have enough balance to pay fee";
+  }
+
+  const sellAmountAfterFee = sellBalance.sub(fee);
+  const buyAmount = await api.estimateTradeAmount(
+    sellToken.address,
+    buyToken.address,
+    sellAmountAfterFee,
+    OrderKind.SELL
   );
 
   console.log(
-    `Selling ${sell_balance.toString()} of ${sell_token.name} for ${
-      buy_token.name
-    }`
+    `Selling ${sellAmountAfterFee.toString()} of ${
+      sellToken.name
+    } for ${buyAmount} of ${buyToken.name} with a ${fee.toString()} fee`
   );
+
+  const order = createOrder(
+    sellToken,
+    buyToken,
+    sellAmountAfterFee,
+    buyAmount,
+    fee
+  );
+  const signature = await signOrder(
+    domain(
+      network.config.chainId,
+      // TODO: How do I get this address from the repo?
+      "0x4E608b7Da83f8E9213F554BDAA77C72e125529d0"
+    ),
+    order,
+    trader,
+    // TODO: TypedData does not seem to work with the ethers provider
+    SigningScheme.MESSAGE
+  );
+
+  const uid = await api.placeOrder(order, signature);
+  console.log(`Successfully placed order with uid: ${uid}`);
 }
 
 async function fetchTokenList(
@@ -58,14 +109,14 @@ interface TokenAndBalance {
   balance: BigNumber;
 }
 
-async function tokensWithBalance(
-  all_tokens: TokenInfo[],
+async function filterTokensWithBalance(
+  allTokens: TokenInfo[],
   trader: SignerWithAddress,
   ethers: HardhatEthersHelpers
 ): Promise<TokenAndBalance[]> {
   return (
     await Promise.all(
-      all_tokens.map(async (token) => {
+      allTokens.map(async (token) => {
         const erc20 = await toERC20(token.address, ethers);
         const balance: BigNumber = await erc20.balanceOf(trader.address);
         return {
@@ -86,4 +137,36 @@ async function toERC20(
   return new Contract(address, ERC20.abi, ethers.provider);
 }
 
-export { makeTrade };
+// Using the most significant 4 bytes of a unique phrase's hash. TODO: use full hash after SC upgrade.
+const APP_DATA = parseInt(
+  ethers.utils.hexDataSlice(keccak("GPv2 Trading Bot"), 0, 4)
+);
+
+function keccak(message: string) {
+  const utf8Encoder = new TextEncoder();
+  return ethers.utils.keccak256(utf8Encoder.encode(message));
+}
+
+function createOrder(
+  sellToken: TokenInfo,
+  buyToken: TokenInfo,
+  sellAmountAfterFee: BigNumber,
+  buyAmount: BigNumber,
+  fee: BigNumber
+): Order {
+  // getTime returns milliseconds, we are looking for seconds
+  const now = Math.floor(new Date().getTime() / 1000);
+  return {
+    sellToken: sellToken.address,
+    buyToken: buyToken.address,
+    sellAmount: sellAmountAfterFee,
+    // add 0.5 % slippage
+    buyAmount: buyAmount.mul(995).div(1000),
+    // valid 15 minutes
+    validTo: now + 900,
+    appData: APP_DATA,
+    feeAmount: fee,
+    kind: OrderKind.SELL,
+    partiallyFillable: false,
+  };
+}
